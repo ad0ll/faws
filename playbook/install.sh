7d94055f12ed62249ebb8d9ec5f6aaac05df9dc7
Parallels <parallels@debian-gnu-linux-11.localdomain>
2022-11-07 07:40:31 -0800

    Playbook tweaks

--- playbook/install.sh
+++ playbook/install.sh
@@ -5,14 +5,13 @@
 # Non-debian systems may work, but note that the node software makes
 # This will be converted to a playbook in the future
 
-#alias install-cmd="apt install -y"
+set -e
 
-#Below will likely prompt the user for the a sudoer password
-#su - root
-
-#sudo apt install -y git curl python3-pip
+apt install -y git curl python3-pip
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+source ~/.bashrc
+source ~/.nvm/nvm.sh
 #PIP_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
-su - root
 COORDINATOR_ID=$(cat ../contract/neardev/dev-account) #TODO Swap this for the actual coordinator id
 PIP_PATH=$(python3 -m site --user-site)
 export PATH="$PATH:$PIP_PATH:$HOME/.local/bin"
@@ -37,24 +36,30 @@ install_ansible() {
   fi
 }
 
+
 echo "Installing ansible..."
 install_pip
 install_ansible
 
-ehco "Installing tools"
-#ansible-playbook install-tools.yaml --become-password-file .password
+
+git clone git@github.com:ad0ll/near-isnt-decentralized.git
+cd near-isnt-decentralized/playbook
+
+
+echo "Installing tools"
+#ansible-playbook install-tools.yaml --ask-become-pass
+ansible-playbook install-tools.yaml --become-password-file .password
 
 #We now have near installed and can check that the provided contract, account id, and node name/id are all valid
 source ~/.nvm/nvm.sh
-#ansible-playbook playbook.yaml --ask-become-pass
 
 #read -s "What is your near account id? (ex: account1.testnet): "
 #read -s "What is the name of your node? (ex: node1): "
-ansible-playbook install-client.yaml --become-password-file .password
+#ansible-playbook install-client.yaml --become-password-file .password
 #Check if key file exists, and if it doesn't, run near login
 #near login --accountId garbage8.testnet
-cd /home/parallels/near-isnt-decentralized2/execution-client
-near login
-pm2 startup
-pm2 restart index
-pm2 save
+#cd /home/parallels/near-isnt-decentralized2/execution-client
+#near login
+#pm2 startup
+#pm2 restart index
+#pm2 save
